6436452bd2c4e3cdfbf664198ef83a61c593f86c
con266667 <cnwilson29@gmail.com>
2024-03-08 12:37:51 -0500

    cleaner

--- civ.py
+++ civ.py
@@ -6,7 +6,7 @@ E_pvc = 3.5 * ureg.gigapascal
 length = 0.5 * ureg.meter
 radius =  1 * ureg.inch
 thickness = 0.5 * ureg.inch
-force = 1 * ureg.gram * ureg.meter / ureg.second**2
+force = 100 * ureg.gram * ureg.meter / ureg.second**2
 
 def pipe_area_moment_of_inertia(radius, thickness):
     return math.pi * (radius**4 - (radius - thickness)**4) / 64
@@ -15,6 +15,4 @@ def pipe_tip_deflection(length, radius, thickness, force, E):
     I = pipe_area_moment_of_inertia(radius, thickness)
     return (force * length**3) / (3 * E * I)
 
-
-# print(pipe_area_moment_of_inertia(radius, thickness))
-print(pipe_tip_deflection(length, radius, thickness, force, E_pvc).to(ureg.millimeter))
+print(round(pipe_tip_deflection(length, radius, thickness, force, E_pvc).to(ureg.millimeter), 3))
